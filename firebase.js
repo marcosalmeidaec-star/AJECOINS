c4c8ce24420149ec5c42bc99324b11fde5ca40a5
marcosalmeidaec-star <marcos.almeida.ec@ajegroup.com>
2026-01-05 07:57:31 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,19 @@
-// Import Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore, collection, setDoc, doc, getDocs } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
+<!-- Firebase SDK -->
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js"></script>
 
-// Configuración de Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCsz2EP8IsTlG02uU2_GRfyQeeajMDuJjI",
-  authDomain: "ajecoins-73829.firebaseapp.com",
-  projectId: "ajecoins-73829",
-  storageBucket: "ajecoins-73829.appspot.com",
-  messagingSenderId: "247461322350",
-  appId: "1:247461322350:web:802185ad39249ca650507f"
-};
+<script>
+  // Configuración
+  const firebaseConfig = {
+    apiKey: "AIzaSyCsz2EP8IsTlG02uU2_GRfyQeeajMDuJjI",
+    authDomain: "ajecoins-73829.firebaseapp.com",
+    projectId: "ajecoins-73829",
+    storageBucket: "ajecoins-73829.appspot.com",
+    messagingSenderId: "247461322350",
+    appId: "1:247461322350:web:802185ad39249ca650507f"
+  };
 
-// Inicializar Firebase
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+  // Inicializar Firebase
+  const app = firebase.initializeApp(firebaseConfig);
+  const db = firebase.firestore();
+</script>
