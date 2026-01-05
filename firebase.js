39e92a0e6ce0f31fff0cb77a43225ea9418e0d4d
marcosalmeidaec-star <marcos.almeida.ec@ajegroup.com>
2026-01-05 00:27:39 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-firestore.js";
 
+// 🔹 Configuración Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCsz2EP8IsTlG02uU2_GRfyQeeajMDuJjI",
   authDomain: "ajecoins-73829.firebaseapp.com",
@@ -10,10 +11,11 @@ const firebaseConfig = {
   appId: "1:247461322350:web:802185ad39249ca650507f"
 };
 
+// 🔹 Inicializar app
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// hacemos db global
+// 🔹 Hacer db accesible globalmente
 window.db = db;
 
 console.log("🔥 Firebase conectado correctamente");
